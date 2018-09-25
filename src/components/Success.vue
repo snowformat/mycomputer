1dc6e2ada1ebf31d47170af25fcaf6b9d85b6f09
yezi <472171252@qq.com>
2018-09-25 14:41:43 +0800

    pcdiercitijiao

--- src/components/Success.vue
+++ src/components/Success.vue
@@ -9,14 +9,26 @@
      <img class="success-pic" src="../../static/images/Group2.png" alt="">
      <h2 class="recharge-success">兑换成功</h2>
      <div class="count">
-         <span>蜻蜓充值账号: 13726873526</span>
-         <span>铂涛充值账号: 13726873526</span>
+         <span>蜻蜓充值账号: {{ QtAccountNum }}</span>
+         <span>铂涛充值账号: {{ BtAccountNum }}</span>
      </div>
      <div class="line"></div>
      <h3>登陆“铂涛旅行”、“蜻蜓FM”，查看更多权益。</h3>
    </div>
 </template>
 <script>
+export default {
+  data () {
+    return {
+      QtAccountNum: '',
+      BtAccountNum: ''
+    }
+  },
+  created () {
+    this.QtAccountNum = localStorage.getItem('qtvalue')
+    this.BtAccountNum = localStorage.getItem('btvalue')
+  }
+}
 </script>
 <style lang="less">
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './assets/css/reset.css'
-import axios from 'axios'
 
-axios.defaults.baseURL = 'https://1411940963167423.cn-hongkong.fc.aliyuncs.com/2016-08-15/proxy/youzan-qingting-wehotel/'
 Vue.config.productionTip = false
-Vue.prototype.$axios = axios
 
 /* eslint-disable no-new */
 new Vue({
